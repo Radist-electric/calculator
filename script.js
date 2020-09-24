4a7d8932cd770bb1a898220c4b38474893a650cd
Radist <radist-electric@yandex.ru>
2020-09-24 16:38:16 +0500

    Completed program

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ let result = 0,
 
 /* Определяем, есть ли фокус на поле ввода */
 num.onfocus = function () {
-  console.log('focused');
   inputFocused = true;
 }
 num.onblur = function () {
-  console.log('blur');
   inputFocused = false;
 }
 
@@ -45,40 +43,31 @@ function changeInput(val) {
 function defineOperation(val) {
   switch (val) {
     case 'div':
-      console.log('Деление');
       divide();
       break;
     case 'mul':
-      console.log('Умножение');
       multiply();
       break;
     case 'minus':
-      console.log('Вычитание');
       minus();
       break;
     case 'plus':
-      console.log('Сложение');
       plus();
       break;
     case 'change':
-      console.log('Смена знака');
       changeSign();
       break;
     case 'dot':
-      console.log('Десятичная точка');
-      dot();
+       dot();
       break;
     case 'back':
-      console.log('Стереть число');
       checkCounted();
       backspace();
       break;
     case 'del':
-      console.log('Сброс полей');
       del();
       break;
     case 'equal':
-      console.log('Вычислить');
       equal();
       break;
     default:
@@ -201,53 +190,40 @@ function checkCounted() {
 /* Проверить, не является ли последний символ в выражении математическим знаком, иначе новый знак не добавляем */
 function checkLastSymbol() {
   let symbol = expOut.textContent[expOut.textContent.length - 1];
-  console.log(symbol);
   return (num.value.length == 0 && (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == undefined));
 }
 
 /* Слушаем нажатие кнопок клавиатуры */
 document.addEventListener('keydown', function (event) {
-  console.log(event.code);
-  console.log(event.key);
   switch (event.key) {
     case 'Enter':
-      console.log('Нажат Enter');
       equal();
       break;
     case '=':
-      console.log('Нажата кнопка Равно');
       equal();
       break;
     case '+':
-      console.log('Нажат +');
       plus();
       break;
     case '-':
-      console.log('Нажат -');
       minus();
       break;
     case '*':
-      console.log('Нажат *');
       multiply();
       break;
     case '/':
-      console.log('Нажат /');
       divide();
       break;
     case 'Backspace':
-      console.log('Нажат backspace');
       backspace();
       break;
     case 'Delete':
-      console.log('Нажата delete');
       del();
       break;
     case '.' || ',':
-      console.log('Нажата точка');
       dot();
       break;
     case 'Control':
-      console.log('Нажат CTRL');
       changeSign();
       break;
     default:
