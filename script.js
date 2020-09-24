ece34c3ddd700f893a2405ee69cd321932573afa
Radist <radist-electric@yandex.ru>
2020-09-24 11:59:58 +0500

    I added a new button and finished all the operations except the dot

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const num = document.querySelector('#number'),
   expOut = document.querySelector('.calc__expression'),
   buttons = document.querySelectorAll('.calc__button');
-let exp = '';
+let result = 0,
+  counted = false;
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
@@ -15,6 +16,11 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 function changeInput(val) {
+  if (counted == true) {
+    num.value = '';
+    expOut.textContent = '';
+    counted = false;
+  }
   num.value = num.value + val;
 }
 
@@ -22,16 +28,38 @@ function defineOperation(val) {
   switch (val) {
     case 'div':
       console.log('Деление');
+      checkCounted();
+      if (num.value[0] == '-') {
+        expOut.textContent = `${expOut.textContent}(${num.value})/`;
+      } else {
+        expOut.textContent = `${expOut.textContent}${num.value}/`;
+      }
+      num.value = '';
       break;
     case 'mul':
       console.log('Умножение');
+      checkCounted();
+      if (num.value[0] == '-') {
+        expOut.textContent = `${expOut.textContent}(${num.value})*`;
+      } else {
+        expOut.textContent = `${expOut.textContent}${num.value}*`;
+      }
+      num.value = '';
       break;
     case 'minus':
       console.log('Вычитание');
+      checkCounted();
+      if (num.value[0] == '-') {
+        expOut.textContent = `${expOut.textContent}(${num.value})-`;
+      } else {
+        expOut.textContent = `${expOut.textContent}${num.value}-`;
+      }
+      num.value = '';
       break;
     case 'plus':
       console.log('Сложение');
-      if(num.value[0] == '-') {
+      checkCounted();
+      if (num.value[0] == '-') {
         expOut.textContent = `${expOut.textContent}(${num.value})+`;
       } else {
         expOut.textContent = `${expOut.textContent}${num.value}+`;
@@ -40,10 +68,10 @@ function defineOperation(val) {
       break;
     case 'change':
       console.log('Смена знака');
-      if(num.value.length > 0 && num.value[0] != '-') {
+      if (num.value.length > 0 && num.value[0] != '-') {
         num.value = '-' + num.value;
       } else {
-         num.value = num.value.substring(1, num.value.length);
+        num.value = num.value.substring(1, num.value.length);
       }
       break;
     case 'dot':
@@ -51,14 +79,36 @@ function defineOperation(val) {
       break;
     case 'del':
       console.log('Стереть число');
+      checkCounted();
       if (num.value.length > 0) {
         num.value = num.value.slice(0, num.value.length - 1);
       }
       break;
+    case 'reset':
+      console.log('Сброс полей');
+      num.value = '';
+      expOut.textContent = '';
+      break;
     case 'equal':
       console.log('Вычислить');
+      if (num.value[0] == '-') {
+        expOut.textContent = `${expOut.textContent}(${num.value})`;
+      } else {
+        expOut.textContent = `${expOut.textContent}${num.value}`;
+      }
+      result = eval(expOut.textContent);
+      num.value = result;
+      expOut.textContent = `${expOut.textContent}=${result}`;
+      counted = true;
       break;
     default:
       console.log('Нет таких значений');
+  }
+}
+
+function checkCounted() {
+  if (counted == true) {
+    expOut.textContent = '';
+    counted = false;
   }
 }
